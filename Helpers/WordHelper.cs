980f658a00fdd6f6accc5a0034dda33fa74985c2
AngelaDemaj <aggelademaj@hotmail.com>
2021-01-23 13:41:17 +0200

    Finished game logic

---
+++ Helpers/WordHelper.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace HangMan.Helpers
+{
+    static class WordHelper
+    {
+        public static string WordToUnderscores(string word)
+        {
+            var text = "";
+
+            foreach (var character in word)
+            {
+                text += "_ ";
+            }
+
+            return text;
+        }
+
+        //Method that checks if the letter we chose exists and where
+        public static HashSet<int> CheckWhereCharacterExists(char character, string word)
+        {
+            var positions = new HashSet<int>();
+
+            if (word.Contains(character))
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == character)
+                    {
+                        positions.Add(i);
+                    }
+                }
+            }
+
+            return positions;
+        }
+
+        //Characters of original word change location like below
+        //0 --> 0
+        //1 --> 2
+        //2 --> 4
+        //3 --> 6
+        //4 --> 8
+        //5 --> 10
+
+        //we need a method that replaces the _ with the character we found
+        public static string ReplaceCharacters(
+            char character, HashSet<int> positions, string word)
+        {
+            char[] characters = word.ToCharArray();
+
+            foreach (var position in positions)
+            {
+                characters[position * 2] = character;
+            }
+            return new string(characters);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,57 +6,61 @@ namespace HangMan
 {
     class Program
     {
-        static string WordToUnderscores(string word)
-        {
-            var text = "";
-
-            foreach (var character in word)
-            {
-                text += "_ ";
-            }
-
-            return text;
-        }
-        //train --> _ _ _ _ _
         //1) we need words replaced by _ _ _ _ _ _ _
         //2) we choose a character each turn
         //--> if the char exists it replaces the underscore _
         //--> if the char does not exist we lose a life
         static void Main(string[] args)
         {
+            PlayHangMan(5);
+        }
+
+        public static void PlayHangMan(int lives)
+        {
             var random = new Random();
             var word = Reader.Words[random.Next(0, Reader.Words.Length)];
-            Console.WriteLine(word);
-            Console.WriteLine(WordToUnderscores(word));
+            var hiddenWord = WordHelper.WordToUnderscores(word);
+            var playerLives = lives;
+            var charactersPlayed = new HashSet<char>();
 
-            var set = CheckWhereCharacterExists('n', "banana");
-            Console.WriteLine();
-
-
-        }
-        //we need a method that checks if the letter we chose exists and where
-        //we need a method that replaces the _ with the character we found in each location given
-        public static HashSet<int> CheckWhereCharacterExists(char character, string word)
-        {
-            var positions = new HashSet<int>();
-
-            if (word.Contains(character))
+            while (playerLives > 0 && hiddenWord.Contains('_'))
             {
-                for (int i = 0; i < word.Length; i++)
+                Console.WriteLine($"Characters already played: " +
+                    $"{string.Join(',', charactersPlayed)}");
+                Console.WriteLine($"Lives Remaining: {playerLives}");
+                Console.WriteLine($"The word is: {hiddenWord}");
+                Console.WriteLine("Enter a character");
+
+                var character = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (charactersPlayed.Contains(character) || !Char.IsLetter(character))
                 {
-                    if (word[i] == character)
+                    Console.Clear();
+                    Console.WriteLine("Choose another character");
+                }
+                else
+                {
+                    charactersPlayed.Add(character);
+                    var set = WordHelper.CheckWhereCharacterExists(character, word);
+
+                    if (set.Count > 0)
                     {
-                        positions.Add(i);
+                        hiddenWord = WordHelper.ReplaceCharacters(character, set, hiddenWord);
+                        Console.Clear();
+                        Console.WriteLine("Character was found");
+                    }
+                    else
+                    {
+                        playerLives--;
+                        Console.Clear();
+                        Console.WriteLine("Character was not found");
                     }
                 }
             }
+            Console.WriteLine(playerLives > 0 ? "You win!" : "You lost!");
 
-            return positions;
-        }
-
-        public static string ReplaceCharacters(char character, HashSet<int> positions)
-        {
-
+            Console.WriteLine($"The word was: {word}");
         }
     }
 }
